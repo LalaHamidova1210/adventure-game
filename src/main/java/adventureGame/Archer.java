0a11ac1aed4883496b2d33e604a11930fb1583f6
Ruslan19-05 <nusrtcrkzov@gmail.com>
2025-01-19 13:46:32 +0400

    rsln commit

--- src/main/java/adventureGame/Archer.java
+++ src/main/java/adventureGame/Archer.java
@@ -1,11 +1,5 @@
 package adventureGame;
 
 public class Archer extends Player {
-    private int id;
-    private String name;
-    private int damage;
-    private int health;
-    private double money;
-
 }
 
