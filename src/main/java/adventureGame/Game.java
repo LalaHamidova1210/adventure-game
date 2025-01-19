42a31d24c0cdaf5005ab04b0a32b0bf353024426
Ruslan19-05 <nusrtcrkzov@gmail.com>
2025-01-19 14:26:18 +0400

    main game adventure app commit

--- src/main/java/adventureGame/Game.java
+++ src/main/java/adventureGame/Game.java
@@ -1,15 +1,22 @@
 package adventureGame;
 
-import com.sun.security.jgss.GSSUtil;
+import java.util.Scanner;
 
 public class Game {
 
     public  void selectCharacter(){
+
+        Scanner sc =new Scanner(System.in);
 
         System.out.println("---------------------------------------");
         System.out.println(new Samurai());
         System.out.println(new Knight());
         System.out.println(new Archer());
         System.out.println("---------------------------------------");
+
+        System.out.print("Select your character:");
+        if(sc.nextInt()==1){
+
+        }
     }
 }
